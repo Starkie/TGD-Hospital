cc6b162e9a69db85511352e6480bf7cca10a6671
Adriano Vega <adrianovega@hotmail.com>
2020-01-05 16:55:06 +0100

    Añadidas las tablas de empleados

--- relational/hospital.sql
+++ relational/hospital.sql
@@ -5,3 +5,21 @@ CREATE TABLE Person (
     FirstSurname VARCHAR2(25) NOT NULL,
     SecondSurname VARCHAR2(25)
 );
+
+CREATE TABLE Employee (
+    ID INTEGER PRIMARY KEY
+    WorksNightShift NUMBER(1) NOT NULL
+
+    CONSTRAINT FK_Person
+        FOREIGN KEY (ID)
+        REFERENCES Person(ID)
+);
+
+CREATE TABLE Patient (
+    ID INTEGER PRIMARY KEY
+    SipNumber VARCHAR2(15) NOT NULL
+
+    CONSTRAINT FK_Person
+        FOREIGN KEY (ID)
+        REFERENCES Person(ID)
+);
